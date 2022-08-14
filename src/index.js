1fc6105094564f26a93ceec917584851516c3deb
kefflen <kefflenjava@hotmail.com>
2022-08-14 13:13:03 -0300

    Usando variavel de ambiente

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import app from "./configs/app.mjs";
 import configs from "./configs/configs.mjs";
 
 
-app.listen(1337, () => {
-  console.log('Running at: http://localhost:8080')
-})
+app.listen(process.env.PORT || 1337, () => {})
