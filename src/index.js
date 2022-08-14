ece2b7a5804d3aa56ed5251582398dfeaa064127
kefflen <kefflenjava@hotmail.com>
2022-08-14 13:08:52 -0300

    Alterando port para 1337

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import app from "./configs/app.mjs";
 import configs from "./configs/configs.mjs";
 
 
-app.listen(8080, () => {
+app.listen(1337, () => {
   console.log('Running at: http://localhost:8080')
 })
