3d21b42aa98a607283011ad688011d880eb8d321
kefflen <kefflenjava@hotmail.com>
2022-08-14 13:18:47 -0300

    Adicionando dotenv

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import app from "./configs/app.mjs";
 import configs from "./configs/configs.mjs";
 
 
-app.listen(process.env.PORT || 1337, () => {})
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Running at: ' + process.env.PORT || 3000)
+})
